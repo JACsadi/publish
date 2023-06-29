e5205252c18d1192f73f96a39b50944168350194
2021331096 <2021331096@student.sust.edu>
2023-06-29 08:04:11 +0600

    aa

---
+++ wouldbeafish.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctime>
+int generateotp() {
+    char arr[9];
+    arr[8] = '\0';
+    int y = 1;
+    for(int i = 0; i < 8; i++) {
+    srand(time(0));
+    int k = rand() % 9;
+     k = (k == 0) ? 1 : k; 
+    y =  (((k + y) % 10) == 0) ? 1 : ((k + y) % 10);
+    arr[i] = (char)(y) + '0';
+    }
+    return atoi(arr);
+}
+int main() {
+    printf("%d", generateotp());
+}
