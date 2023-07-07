ddc55cb71154ea76e7fa0fbf815b6d97426253ca
2021331096 <2021331096@student.sust.edu>
2023-07-07 21:42:23 +0600

    florida kilos

--- shadesofcool.c
+++ shadesofcool.c
@@ -40,3 +40,20 @@ int main () {
    printf("500 miles");
    return 0;
 }
+// the answer should only be this part 
+// char nth_digit_of_the_seq(int n) {
+//  int j = 1;
+//  int count = 0;
+//  while(1) {
+//     int temp =  15 * pow(16, j-1)* j;
+//     if(count + temp > n) break;
+//     count += temp;
+//     j++; 
+//  }
+//    if(n == count) return 'F';
+//    n -= count;
+//    int num = pow(16, j - 1) + (int)(ceil((double)n/(double)j)) - 1;
+//    char *arr = (char*) malloc((j+1)*sizeof(char));
+//    sprintf(arr, "%X", num);
+//    return arr[((n%j) == 0 ) ? j-1 : (n%j)-1];
+// }
